a1b9e02c86a87c6d4b9347077936fa41e7d14480
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-05 16:39:25 -0600

    Close buffers when testing to prevent warnings

--- read_transcript.py
+++ read_transcript.py
@@ -23,6 +23,10 @@ class Transcript:
         raise Exception("Student Name Not Found!")
 
 
+    def close(self):
+        self.transcript.close()
+
+
     def _get_first_page_words(self):
         first_page = self._get_first_page()
         first_page_words = first_page.extract_words(x_tolerance=30, y_tolerance=3, keep_blank_chars=True, use_text_flow=False, horizontal_ltr=True, vertical_ttb=True, extra_attrs=[], split_at_punctuation=False)
