fede7c7b0bc1835dc5b9f77c8ad6477538a1e845
Lisa Chen <bao.lisa.chen@gmail.com>
2023-03-15 04:48:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,88 @@ import os
 import sys
 import tkinter as tk
 from tkinter import *  # tkinter package for GUI purposes
-from tkinter import filedialog  # tkinter filedialog package
+from tkinter import ttk, filedialog, messagebox as mbox
+from tkinter.messagebox import showinfo
 
-# Gets file name from file explorer and opens it
-def file_selection():
-    file_name = filedialog.askopenfilename(initialdir="/",
-                                          title="Select file",
-                                          filetypes=(("All Files","*.*"),("Adobe Acrobat Document","*.pdf*"),("Microsoft Word Document","*.docx*"),("Microsoft Excel Worksheet","*.xlsx")))
 
-    # Opens file in read-only mode just in case file_name doesn't do so
+# Opens file and displays an error message if it fails
+def open_file(file_name):
+    file_err_msg = "File not opened or could not be opened."
+
+    # Opens file in read-only mode
     try:
-        fp=open(file_name,"r")  # opens file in read-only mode
-        fp.close()  # closes file
-        print("User selected the file: " + file_name) # prints in terminal the path of opened file
+        with open(file_name, "r") as f:
+            # Displays selected file name to user
+            showinfo(
+                title="Selected File",
+                message="Selected file: "+get_filename(file_name)
+            )
     except:
-        print("File count not be opened ")  # prints error message of file couldn't be opened
-        print(sys.exit())  # exits system
+        mbox.showerror("Error", file_err_msg)  # failed opening file message box
+
+# Gets file name from file path
+def get_filename(file_name):
+    filename = os.path.basename(file_name)
+    return filename
+
+# Gets file path of chosen file selected from file explorer
+def file_selection():
+    pdf = "Adobe Acrobat Document"
+    word_doc = "Microsoft Word Document"
+    excel_sheet = "Microsoft Excel Worksheet"
+    file_explorer = "/"  # path start for windows
+    finder = "\\"  # path start for mac OS
+
+    # opens Windows File Explorer and gets file path
+    file_name = filedialog.askopenfilename(initialdir=file_explorer, title="Select file",
+                                           filetypes=(("All Files", "*.*"), (pdf, "*.pdf*"),
+                                                      (word_doc, "*.docx*"), (excel_sheet, "*.xlsx")))
+
+    open_file(file_name)  # calls function to open file
+
 
 # file selection gui for the window and browse file button
-def file_select_gui(file_selection):
-    window = Tk()  # makes Window
-    window.config(background="white")  # Window background color
-    window.geometry("700x500")  # Window size
-    window.title('Degree Plan and Audit Tool') # Window name
+def file_select_gui(window):
+    # Upload File text label and design
+    lbl_upload = Label(
+        window,
+        text="Upload File",
+        font="Roboto 15",
+        width=100,
+        height=4,
+        fg="black",
+        bg="white"
+    )
+    lbl_upload.pack(pady=10)  # text padding
 
-    button_file_browse = Button(window, text="Browse Files", command=file_selection)  # File Browse button to open file
-    button_file_browse.pack(side = 'top') # places File Browse button near the top of window
+    # Button to open Window's File Explorer and design
+    btn_file_browse = Button(
+        window,
+        text="Browse Files",
+        command=file_selection
+    )
+    btn_file_browse.pack(side=TOP, pady=20)  # button padding
 
-    window.mainloop()  # waits for user to finish selecting file
+
+# Sets up application window
+def window_gui():
+    window_name = "Degree Plan and Audit Tool"
+    window_size = "700x500"
+    window_color = "white"
+
+    window = Tk()  # makes a tkinter frame instance Window
+    window.config(background=window_color)  # Window background color
+    window.geometry(window_size)  # Window size
+    window.title(window_name)  # Window name
+
+    file_select_gui(window)  # file selection window gui
+
+    window.mainloop()  # keeps window open until exited
+
+
+def main():
+    window_gui()
+
 
 if __name__ == '__main__':
-    file_select_gui(file_selection) # calling file selection gui function
-
-
+    main()
