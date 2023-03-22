0a12cc94d1a31b5b32c2d39e7e0340770c17f57b
alekhyapinnamaneni <72823695+alekhyapinnamaneni@users.noreply.github.com>
2023-03-22 09:13:41 -0500

    Update Student_Object.py

--- Student_Object.py
+++ Student_Object.py
@@ -22,26 +22,25 @@
 #           'Points': 97.02
 #       })
 class Student:
-    def __init__(self, student_id, name, program, major, courses, cum, transfer_cum, comb_cum):
+    def __init__(self, student_id, name, program, major, courses, cumulative, transfer_cumulative, combined_cumulative):
         self.student_id = student_id
         self.name = name
         self.program = program
         self.major = major
         self.courses = courses
-        self.cum = cum
-        self.transfer_cum = transfer_cum
-        self.comb_cum = comb_cum
+        self.cumulative = cumulative
+        self.transfer_cumulative = transfer_cumulative
+        self.combined_cumulative = combined_cumulative
 
 
 # Course data object
 # ex: Course('CS', 6360, 'DATABASE DESIGN', ['Murat Kantarcioglu', 'Mestan Firat Celiktug'], 3.0, 3.0, 'A-', 11.01)
 class Course:
-    def __init__(self, dept, code, name, professors, total_credits, earned_credits, letter_grade, points):
+    def __init__(self, dept, code, name, attempted_credits, earned_credits, letter_grade, points):
         self.dept = dept
         self.code = code
         self.name = name
-        self.professors = professors
-        self.total_credits = total_credits
+        self.attempted_credits = attempted_credits
         self.earned_credits = earned_credits
         self.letter_grade = letter_grade
         self.points = points
