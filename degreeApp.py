73d7605b4a69b123b8b27138a523caa5dd8c5792
Lisa Chen <bao.lisa.chen@gmail.com>
2023-03-24 23:49:00 -0500

    Update degreeApp.py

--- degreeApp.py
+++ degreeApp.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 
-# Handles the application window
+
 class DegreeApp(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -11,6 +10,7 @@ class DegreeApp(tk.Tk):
         window_size = "760x550+270+25"
         window_color = "white"
 
+        # Handles the application window
         self.config(background=window_color)  # Window background color
         self.geometry(window_size)  # Window size
         self.title(window_name)  # Window name
