764b1cd0c82b2cb52720c2fe8c59c4a4019d7d77
Lisa Chen <bao.lisa.chen@gmail.com>
2023-03-24 23:50:43 -0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -3,31 +3,41 @@ from tkinter import *
 from tkinter import ttk
 from degreeApp import DegreeApp
 
+
 class Homepage(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
-        self.pack(fill=BOTH, expand=True) # assigns ttk.Frame to root application window
-        self.homepage_gui()
+        self.pack(fill=BOTH, expand=True)  # assigns ttk.Frame to root application window
 
-    # Handles the gui and function of the application homepage
-    def homepage_gui(self):
-        self.style = ttk.Style(self)
-        self.style.configure("BW.TLabel", font=("Roboto", 30), foreground="black", background="white")
+        self.style = ttk.Style(self)  # keep here or in homepage_gui function
+
+        # Handles the gui and function of the application homepage
         self.style.configure("BlBord.TFrame", borderwidth=5, background="white", relief=SOLID, height=60, width=70)
+        self.style.configure("BW.TLabel", font=("Segoe Print", 30), foreground="#1496BB", background="white")
+        self.style.configure("StartBig.TButton", font=("Segoe UI", 16), foreground="#ffffff", background="#086623")
 
         # Frame outline design
-        self.frame = ttk.Frame(self, style="BlBord.TFrame")
-        self.frame["padding"] = (5, 0, 5, 0)  # adjusts inner padding to fit text
-        self.frame.pack(fill=BOTH, expand=False, pady=20)  # frame padding
+        frame = ttk.Frame(self, style="BlBord.TFrame")
+        frame["padding"] = (5, 0, 5, 0)  # adjusts inner padding to fit text
+        frame.pack(fill=BOTH, expand=False, pady=90)  # frame padding
 
         # Homepage title label and design
-        lbl_upload = ttk.Label(self.frame, text="Degree Plan and Audit Tool", style="BW.TLabel")
-        lbl_upload.pack(fill=NONE, expand=False, padx=30, pady=10)  # text padding
+        lbl_upload = ttk.Label(frame, text="Degree Plan and Audit Tool", style="BW.TLabel")
+        lbl_upload.pack(fill=NONE, expand=False, padx=30, pady=30)  # text padding
 
         # Homepage Start button and design
-        btn_file_browse = ttk.Button(
-            self.frame,
-            text="+ Start",
-            command=self.file_selection
-        )
-        btn_file_browse.pack(side=TOP, pady=20)  # button padding
+        #start_btn = ttk.Button(
+        #    frame,
+        #    text="+ Start",
+        #    style="StartBig.TButton",
+        #    command=self.next_page
+        #)
+        #start_btn.pack(side=TOP, pady=20)  # button padding
+
+    def next_page(self):
+        print("Modify this later")
+
+        #  Segoe UI, bookman old style, bradley hand ITC, ink free, segoe print, candara light, century gothic
+        #  003300, 003333, 006600, 006633, 086623 (green),
+        #  107896, 1287A8, 43ABC9, #4582ec, 1496BB (blue),
+        #  #593196, 7442C8, 800080, 575068 (purple)
