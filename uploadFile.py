e2966a3e957fd78ed19b956c9af21f87ad8c60d2
Lisa Chen <bao.lisa.chen@gmail.com>
2023-03-31 02:30:18 -0500

    Update uploadFile.py

--- uploadFile.py
+++ uploadFile.py
@@ -1,64 +1,93 @@
 import os
-import sys
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk, filedialog, messagebox as mbox
-from tkinter.messagebox import showinfo
-from degreeApp import DegreeApp
+from tkinter import ttk, filedialog
+from tkinter import messagebox as mbox
 
-class UploadFile(ttk.Frame):
-    def __init__(self, container):
+
+class UploadFilePage(ttk.Frame):
+    def __init__(self, container, controller):
         super().__init__(container)
-        self.pack(fill="both", expand=True) # assigns ttk.Frame to root application window
-        self.filename = tk.StringVar()
-        self.file_select_gui()
+        self.controller = controller  # Used to control which page is shown
 
+        self.filename = tk.StringVar()  # Holds displayed name of file
+        self.file_path = ""
 
-    # Handles the gui of the Upload File page
-    def file_select_gui(self):
-        self.style = ttk.Style(self)
-        self.style.configure("BW.TLabel", font=("Roboto", 15), foreground="black", background="white")
-        self.style.configure("GWSmall.TLabel", font=("Roboto", 12), foreground="Gray", background="white")
-        self.style.configure("BWSmall.TLabel", font=("Roboto", 12), foreground="Blue", background="white")
-        self.style.configure("BlBord.TFrame", borderwidth=5, background="white", relief=SOLID, height=60, width=70)
-        self.style.configure("picBkgd.TLabel", background="white")
+        # Handles the gui of the Upload File page
+        style = ttk.Style(self)
+        style.configure("BlBord.TFrame", borderwidth=5, background="white", relief=SOLID, height=500, width=700)
+        style.configure("picBkgd.TLabel", background="white")
+        style.configure("BW.TLabel", font=("Roboto", 20), foreground="black", background="white")
+        style.configure("GWSmall.TLabel", font=("Roboto", 14), foreground="Gray", background="white")
+        style.configure("BWSmall.TLabel", font=("Roboto", 14), foreground="#107896", background="white")
 
-        # Frame outline design
-        self.frame = ttk.Frame(self, style="BlBord.TFrame")
-        self.frame["padding"] = (5, 0, 5, 0)  # adjusts inner padding to fit text
-        self.frame.pack(fill=BOTH, expand=False, pady=20)  # frame padding
+        # Handles frame expansion when application window is expanded
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        # Frame outline and design
+        frame = ttk.Frame(self, style="BlBord.TFrame")
+        frame["padding"] = (5, 0, 5, 0)  # adjusts inner padding to fit text
+        frame.grid(column=0, row=0, sticky="nsew", columnspan=5)  # frame placement
+
+        # Handles frame's space distribution
+        frame.grid_columnconfigure(0, weight=1)
+        frame.grid_rowconfigure(0, weight=1)
+        frame.grid_columnconfigure(1, weight=1)
+        frame.grid_rowconfigure(1, weight=1)
+        frame.grid_columnconfigure(2, weight=1)
+        frame.grid_rowconfigure(2, weight=1)
+        frame.grid_columnconfigure(3, weight=1)
+        frame.grid_rowconfigure(3, weight=1)
+        frame.grid_columnconfigure(4, weight=1)
+        frame.grid_rowconfigure(4, weight=1)
+        frame.grid_rowconfigure(5, weight=1)
 
         # Decorative Image label and design
-        self.doc_upload_photo = tk.PhotoImage(file=r"./images/upload_file.png")
-        self.lbl_image = ttk.Label(
-            self.frame,
-            image=self.doc_upload_photo,
+        doc_upload_photo = tk.PhotoImage(file=r"./images/upload_file.png")
+        lbl_image = ttk.Label(
+            frame,
+            image=doc_upload_photo,
             style="picBkgd.TLabel"
         )
-        self.lbl_image.doc_upload_photo = self.doc_upload_photo  # Reference of Image to display image
-        self.lbl_image.pack(side=TOP, fill=NONE, expand=5, padx=(20,0), pady=(20, 10))  # padding
+        lbl_image.doc_upload_photo = doc_upload_photo  # Required image reference needed for image to show up
+        lbl_image.grid(column=1, row=0, columnspan=3, pady=(10, 0))  # positioning
 
         # Upload File text label and design
-        lbl_upload = ttk.Label(self.frame, text="Upload File", style="BW.TLabel")
-        lbl_upload.pack(fill=NONE, expand=False, padx=30, pady=10)  # text padding
+        lbl_upload = ttk.Label(frame, text="Upload File", style="BW.TLabel")
+        lbl_upload.grid(column=2, row=1, columnspan=1, pady=(5, 5))  # positioning
 
         # subtext label and design
-        lbl_upload = ttk.Label(self.frame, text="Click the button to select a file to upload: ",
-                                    style="GWSmall.TLabel")
-        lbl_upload.pack(fill=NONE, expand=False, padx=20, pady=10)  # text padding
+        lbl_upload_instruct = ttk.Label(frame, text="Click the button to select a file to upload", style="GWSmall.TLabel")
+        lbl_upload_instruct.grid(column=1, row=2, columnspan=3, pady=(10, 0))  # positioning
 
-        # Open Window's File Explorer button and design
+        # Window's File Explorer button and design
         btn_file_browse = ttk.Button(
-            self.frame,
+            frame,
             text="Browse Files",
             command=self.file_selection
         )
-        btn_file_browse.pack(side=TOP, pady=20)  # button padding
+        btn_file_browse.grid(column=2, row=3, columnspan=1, pady=(15, 5))  # positioning
 
-        #file name (not file path) text label and design
-        self.file_name = ttk.Label(self.frame, textvariable=self.filename, style="BWSmall.TLabel")
-        self.file_name.pack(pady=10)
+        # file name (not file path) text label and design
+        self.file_name = ttk.Label(frame, textvariable=self.filename, style="BWSmall.TLabel")
+        self.file_name.grid(column=0, row=4, columnspan=5, padx=(0, 5), pady=(10, 10))  # positioning
 
+        # Previous page button and design
+        prev_btn = ttk.Button(
+            frame,
+            text="<< Previous",
+            command=lambda: self.controller.show_frame("HomepageStart")
+        )
+        prev_btn.grid(column=0, row=5, sticky="sw", columnspan=1, padx=(5, 0), pady=(10, 20))  # positioning
+
+        # Next page button and design
+        next_btn = ttk.Button(
+            frame,
+            text="Next >>",
+            command=lambda: self.controller.show_frame("DegreePlanPage")
+        )
+        next_btn.grid(column=5, row=5, sticky="es", columnspan=1, padx=(0, 5), pady=(10, 20))  # positioning
 
     # Opens file in read-only mode and returns if it's successful or not
     def open_file(self):
@@ -68,21 +97,28 @@ class UploadFile(ttk.Frame):
         else:
             return False
 
+    # Sets file path object
+    def set_filepath(self, file_path):
+        self.file_path = file_path
 
     # Gets file path of file selected from file explorer by the user and opens it
     def file_selection(self):
         pdf = "Adobe Acrobat Document"
         word_doc = "Microsoft Word Document"
         excel_sheet = "Microsoft Excel Worksheet"
+        json = "JavaScript Object Notation File"
         file_explorer = "\\"  # path start for windows
         file_err_msg = "File could not be opened or was not chosen."
 
         # opens Windows File Explorer and gets file path of the selected file
-        self.file_path = filedialog.askopenfilename(initialdir=file_explorer, title="Open",
+        file_path = filedialog.askopenfilename(initialdir=file_explorer, title="Open",
                                                filetypes=(("All Files", "*.*"), (pdf, "*.pdf*"),
-                                                          (word_doc, "*.docx*"), (excel_sheet, "*.xlsx")))
+                                                          (word_doc, "*.docx*"), (excel_sheet, "*.xlsx"),
+                                                          (json, ".json")))
 
-        file_status = UploadFile.open_file(self)  # calls function to open file
+        self.set_filepath(file_path)  # sets file path as instance variable/object
+
+        file_status = self.open_file()  # calls function to open file
 
         # Displays name of file that user selected and opened
         if file_status:
@@ -90,3 +126,7 @@ class UploadFile(ttk.Frame):
             self.filename.set(file_name)
         else:
             mbox.showerror("Error", file_err_msg)  # failed opening file message box
+
+    # Gets file path object/instance variable
+    def get_filepath(self):
+        return self.file_path
