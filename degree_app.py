3f01d32647436caab0b5cdfc60885b175eb39cf0
Lisa Chen <bao.lisa.chen@gmail.com>
2023-04-01 19:47:59 -0500

    Add files via upload

---
+++ degree_app.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from upload_file_page import UploadFilePage
+from homepage import HomepageStart
+from degree_plan_page import DegreePlanPage
+from audit_report_page import AuditReportPage
+
+
+class DegreeApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        window_name = "Degree Plan and Audit Tool"
+        window_size = "760x550+270+25"
+        window_color = "white"
+        self.frames = {}  # initializes application page to array
+
+        # Sets up the application window
+        self.config(background=window_color)  # Window background color
+        self.geometry(window_size)  # Window size
+        self.title(window_name)  # Window name
+        icon = tk.PhotoImage(file="./images/project_icon.png")
+        self.iconphoto(FALSE, icon)  # window icon logo
+
+        # Sets up application frame container and placement
+        container = ttk.Frame(self)
+        container.pack(expand=TRUE, fill=BOTH, side=TOP)
+        container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+
+        # Handles switching between application pages using Class Names
+        for class_Name in (HomepageStart, UploadFilePage, DegreePlanPage, AuditReportPage):
+            page_name = class_Name.__name__
+            frame = class_Name(container, self)  # creates instance of each class where self=controller
+            frame.grid(column=0, row=0, sticky="nsew")  # keeps pages bundled when moving the Window
+            self.frames[page_name] = frame  # initializes classes
+
+        self.show_frame("HomepageStart")  # Displays application homepage
+
+    # Displays page the user navigated to
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
---
+++ upload_file_page.py
@@ -0,0 +1,125 @@
+import os
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk, filedialog
+from tkinter import messagebox as mbox
+
+
+class UploadFilePage(ttk.Frame):
+    def __init__(self, container, controller):
+        super().__init__(container)
+        self.controller = controller  # Used to control which page is shown
+
+        self.filename = tk.StringVar()  # Holds displayed name of file
+        self.file_path = ""
+
+        # Handles the gui of the Upload File page
+        style = ttk.Style(self)
+        style.configure("BlBord.TFrame", borderwidth=5, background="#f8f8ff", relief=SOLID)
+        style.configure("pic_background.TLabel", background="#f8f8ff")
+        style.configure("BW.TLabel", font=("Roboto", 20), foreground="black", background="#f8f8ff")
+        style.configure("GWSmall.TLabel", font=("Roboto", 14), foreground="Gray", background="#f8f8ff")
+        style.configure("BWSmall.TLabel", font=("Roboto", 14), foreground="#107896", background="#f8f8ff")
+
+        # Handles frame expansion when application window is expanded
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        # Frame outline and design
+        frame = ttk.Frame(self, style="BlBord.TFrame")
+        frame["padding"] = (5, 0, 5, 0)  # adjusts inner padding to fit text
+        frame.grid(column=0, row=0, sticky="nsew", columnspan=5)  # frame placement
+
+        # Handles frame's page space distribution
+        for row_index in range(6):
+            frame.grid_rowconfigure(row_index, weight=1)
+        for col_index in range(5):
+            frame.grid_columnconfigure(col_index, weight=1)
+
+        # Decorative Image label and design
+        doc_upload_photo = tk.PhotoImage(file=r"./images/upload_file.png")
+        lbl_image = ttk.Label(
+            frame,
+            image=doc_upload_photo,
+            style="pic_background.TLabel"
+        )
+        lbl_image.doc_upload_photo = doc_upload_photo  # image reference needed for image to load
+        lbl_image.grid(column=1, row=0, columnspan=3, pady=(10, 0))  # positioning
+
+        # Upload File text label and design
+        lbl_upload = ttk.Label(frame, text="Upload File", style="BW.TLabel")
+        lbl_upload.grid(column=2, row=1, columnspan=1, pady=(5, 5))  # positioning
+
+        # subtext label and design
+        upload_instruct = "Click the button to select a file to upload"
+        lbl_upload_instruct = ttk.Label(frame, text=upload_instruct, style="GWSmall.TLabel")
+        lbl_upload_instruct.grid(column=1, row=2, columnspan=3, pady=(10, 0))  # positioning
+
+        # Window's File Explorer button and design
+        btn_file_browse = ttk.Button(
+            frame,
+            text="Browse Files",
+            command=self.file_selection
+        )
+        btn_file_browse.grid(column=2, row=3, columnspan=1, pady=(15, 5))  # positioning
+
+        # file name (not file path) text label and design
+        self.file_name = ttk.Label(frame, textvariable=self.filename, style="BWSmall.TLabel")
+        self.file_name.grid(column=0, row=4, columnspan=5, padx=(0, 5), pady=(10, 10))  # positioning
+
+        # Previous page button and design
+        prev_btn = ttk.Button(
+            frame,
+            text="<< Previous",
+            command=lambda: self.controller.show_frame("HomepageStart")
+        )
+        prev_btn.grid(column=0, row=5, sticky="sw", columnspan=1, padx=(5, 0), pady=(10, 20))  # positioning
+
+        # Next page button and design
+        next_btn = ttk.Button(
+            frame,
+            text="Next >>",
+            command=lambda: self.controller.show_frame("DegreePlanPage")
+        )
+        next_btn.grid(column=5, row=5, sticky="es", columnspan=1, padx=(0, 5), pady=(10, 20))  # positioning
+
+    # Opens file in read-only mode and returns if it's successful or not
+    def open_file(self):
+        if self.file_path:
+            with open(self.file_path, "r") as f:
+                return True
+        else:
+            return False
+
+    # Sets file path object
+    def set_filepath(self, file_path):
+        self.file_path = file_path
+
+    # Gets file path of file selected from file explorer by the user and opens it
+    def file_selection(self):
+        pdf = "Adobe Acrobat Document"
+        word_doc = "Microsoft Word Document"
+        excel_sheet = "Microsoft Excel Worksheet"
+        json = "JavaScript Object Notation File"
+        file_explorer = "\\"  # path start for windows
+        file_err_msg = "File could not be opened or was not chosen."
+        file_types = [("All Files", "*.*"), (pdf, "*.pdf*"), (word_doc, "*.docx*"), (excel_sheet, "*.xlsx"),
+                     (json, ".json")]
+
+        # opens Windows File Explorer and gets file path of the selected file
+        file_path = filedialog.askopenfilename(initialdir=file_explorer, title="Open", filetypes=file_types)
+
+        self.set_filepath(file_path)  # sets file path as instance variable/object
+
+        file_status = self.open_file()  # calls function to open file
+
+        # Displays name of file that user selected and opened
+        if file_status:
+            file_name = "Uploaded File: " + os.path.basename(self.file_path)
+            self.filename.set(file_name)
+        else:
+            mbox.showerror("Error", file_err_msg)  # File not opened message box alert
+
+    # Gets file path object/instance variable
+    def get_filepath(self):
+        return self.file_path
