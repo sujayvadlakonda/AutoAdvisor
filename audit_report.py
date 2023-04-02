595688ff6f59af38692f6810ed097d1f274d49d2
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-04-02 17:29:15 -0500

    See if basic core requirements are fulfilled

--- audit_report.py
+++ audit_report.py
@@ -1,7 +1,36 @@
-
-
 class AuditReport:
-    def __init__(self):
-        super().__init__()
+    def __init__(self, courses, requirements):
+        self.courses = courses
+        self.requirements = requirements
 
-    # Insert audit report related functions here
+    def get_core_fulfilled(self):
+        core_fulfilled = []
+
+        for course in self.courses:
+            course_full_id = (
+                course["subject"].strip() + " " + course["course_id"].strip()
+            )
+            if self.requirements.is_requirement(course_full_id):
+                core_fulfilled.append(course_full_id)
+
+        return core_fulfilled
+
+
+class CoreRequirements:
+    requirements = []
+
+    def is_requirement(self, course):
+        if course in self.requirements:
+            return True
+
+        return False
+
+
+class DataScience(CoreRequirements):
+    requirements = [
+        "CS 6313",
+        "CS 6350",
+        "CS 6363",
+        "CS 6375",
+        ["CS 6301", "CS 6320", "CS 6327", "CS 6347", "CS 6360"],
+    ]
---
+++ test_audit_report.py
@@ -0,0 +1,17 @@
+import unittest
+
+from course_finder import get_courses
+from audit_report import AuditReport, DataScience
+
+class TestAuditReport(unittest.TestCase):
+    def test_ted_lasso(self):
+        courses = get_courses("transcripts/ted-lasso.pdf")
+        requirements = DataScience()
+        audit_report = AuditReport(courses, requirements)
+        core_fulfilled = audit_report.get_core_fulfilled()
+        expected = ["CS 6363", "CS 6313", "CS 6350", "CS 6375"]
+        self.assertEqual(core_fulfilled, expected)
+
+
+
+unittest.main()
