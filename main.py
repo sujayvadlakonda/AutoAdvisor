934a056dd3d2dac995a0eae9da7efb8e5052ffb5
Leo Nguyen <lichnguyenz@gmail.com>
2023-04-03 09:03:56 -0500

    update main

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from degreeApp import DegreeApp
-from uploadFile import UploadFilePage
-from homepage import HomepageStart
-from degreePlan import DegreePlanPage
-from auditReport import AuditReportPage
-from transcript import *
-# from uploadFile import UploadFilePage
 
 if __name__ == '__main__':
     app = DegreeApp()  # application window instance
