609a964f8e6d99baf6670d572162662312e5f74f
Arno1013 <104392397+Arno1013@users.noreply.github.com>
2023-04-13 11:56:48 -0500

    Add files via upload

---
+++ degree_tracks.py
@@ -0,0 +1,14 @@
+# Outstanding credits
+prerequisites = [5303, 5330, ]
+cores = [6363, 6378, ]
+
+# Choice Credits
+choices = [2, 6353, 6360, 6371]
+
+# Elective Credits
+guided = [15, 6000, 6359]  # must be 6000+, can not include 6359
+electives = [3, ]
+
+track1 = [prerequisites, cores, choices, electives]
+
+degrees = [track1, ]
