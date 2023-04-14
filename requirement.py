04a38d7227873bf19dd8112d7f175765e474747b
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-04-14 13:57:45 -0500

    superior names

--- requirement.py
+++ requirement.py
@@ -1,34 +1,23 @@
-# An abstract class. Basically a requirement class must have a fulfill method.
 class Requirement:
-    # This method should return the course that fulfills this Requirement
-    # If no such course exists it should return false
-    # Better name *highly* welcome
-    def fulfill(self, completed_courses):
+    def is_met(self, completed_courses):
         pass
 
 
-# A requirement that is fulfilled by exactly one course
-# Better name ideas are welcome
-class ExactRequirement(Requirement):
+class SimpleRequirement(Requirement):
     def __init__(self, course):
         self.course = course
 
-    def fulfill(self, completed_courses):
-        # more pythonic way?
-        if self.course in completed_courses:
-            return self.course
-
-        return False
+    def is_met(self, courses):
+        return courses.contains(self.course)
 
 
-# A requirement that is fulfilled by one of several courses
 class MultiRequirement(Requirement):
-    def __init__(self, course_options):
-        self.course_options = course_options
+    def __init__(self, options):
+        self.options = options
 
-    def fulfill(self, completed_courses):
-        for course in self.course_options:
-            if course in completed_courses:
-                return course
+    def is_met(self, courses):
+        for option in self.options:
+            if courses.contains(option):
+                return option
 
         return False
