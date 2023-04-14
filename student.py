82868014b41a3de8f06f0f7702bc93a490bb21f2
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-04-14 13:38:09 -0500

    separate student and course classes

--- student.py
+++ student.py
@@ -33,14 +33,3 @@ class Student:
         self.combined_cumulative = combined_cumulative
 
 
-# Course data object
-# ex: Course('CS', 6360, 'DATABASE DESIGN', ['Murat Kantarcioglu', 'Mestan Firat Celiktug'], 3.0, 3.0, 'A-', 11.01)
-class Course:
-    def __init__(self, dept, code, name, attempted_credits, earned_credits, letter_grade, points):
-        self.dept = dept
-        self.code = code
-        self.name = name
-        self.attempted_credits = attempted_credits
-        self.earned_credits = earned_credits
-        self.letter_grade = letter_grade
-        self.points = points
