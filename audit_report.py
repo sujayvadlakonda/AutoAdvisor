2b2e80998c0d1c074229a7c33561ccf81020a7f6
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-04-15 11:33:46 -0500

    Make audit report construct with transcript and track

--- audit_report.py
+++ audit_report.py
@@ -1,14 +1,4 @@
 class AuditReport:
-    def __init__(self, courses, requirements):
-        self.courses = []
-        for course in courses:
-            course_subject = course["subject"].strip()
-            course_number = course["course_id"].strip()
-            course_identifier = course_subject + " " + course_number
-            self.courses.append(course_identifier)
-        self.requirements = requirements
-
-        self.requirements.fulfill(self.courses)
-
-    def core_completed(self):
-        return self.requirements.core
+    def __init__(self, transcript, track):
+        self.transcript = transcript
+        self.track = track
