c2c2cd6dd6c47a2af256b0fa97b3a8daf2380a4e
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-04-15 12:11:37 -0500

    audit_report: Output core courses

--- track.py
+++ track.py
@@ -1,31 +1,17 @@
+from requirement import SimpleRequirement, MultiRequirement
+
+
 class Track:
     def __init__(self):
         self.core_requirements = []
-        self.core = []
-        self.electives = []
-        self.unfulfilled_requirements = []
-
-    def fulfill(self, completed_courses):
-        for requirements in self.core:
-            pass
-        for requirements in self.electives:
-            pass
-        for requirement in self.requirements:
-            fulfilled_course = requirement.fulfill(completed_courses)
-            if fulfilled_course:
-                self.core.append(fulfilled_course)
-            else:
-                self.unfulfilled_requirements.append(requirement)
 
 
 class DataScience(Track):
     def __init__(self):
-        super().__init__()
-
         self.core_requirements = [
-            ExactRequirement("CS 6313"),
-            ExactRequirement("CS 6350"),
-            ExactRequirement("CS 6363"),
-            ExactRequirement("CS 6375"),
+            SimpleRequirement("CS 6313"),
+            SimpleRequirement("CS 6350"),
+            SimpleRequirement("CS 6363"),
+            SimpleRequirement("CS 6375"),
             MultiRequirement(["CS 6301", "CS 6320", "CS 6327", "CS 6347", "CS 6360"]),
         ]
