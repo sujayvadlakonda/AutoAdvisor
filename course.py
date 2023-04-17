80ad2e69f7f9fc35d1408e8c0d55827d24199702
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-04-17 14:37:54 -0500

    Add leveling courses

--- course.py
+++ course.py
@@ -1,16 +1,27 @@
-# Course data object
-# ex: Course('CS', 6360, 'DATABASE DESIGN', ['Murat Kantarcioglu', 'Mestan Firat Celiktug'], 3.0, 3.0, 'A-', 11.01)
+# Add parameters iff they are used somewhere
 class Course:
-    def __init__(
-        self, dept, code, name, attempted_credits, earned_credits, letter_grade, points
-    ):
-        self.dept = dept
-        self.code = code
-        self.name = name
-        self.attempted_credits = attempted_credits
-        self.earned_credits = earned_credits
-        self.letter_grade = letter_grade
-        self.points = points
+    # id should be "CS 1234"
+    def __init__(self, id):
+        self.id = id
+
+    def __repr__(self):
+        return self.id
+
+
+class LevelingCourseStatus:
+    COMPLETED = "Completed"
+    WAIVED = "Waived"
+    NOT_REQUIRED = "Not required by plan or elective"
+    OTHER = "Other"
+
+
+class LevelingCourse(Course):
+    def __init__(self, id, status=LevelingCourseStatus.COMPLETED):
+        super().__init__(id)
+        self.status = status
+
+    def print(self):
+        print(self.id, self.status)
 
 
 class Courses:
---
+++ demo_leveling_courses.py
@@ -0,0 +1,5 @@
+from track import DataScience
+
+track = DataScience()
+for leveling_course in track.leveling_courses:
+    leveling_course.print()
