78b1cab6a92b67512b55f457e488aca0032a8f0c
Arno1013 <104392397+Arno1013@users.noreply.github.com>
2023-04-19 12:06:31 -0500

    Add files via upload

--- transcript.py
+++ transcript.py
@@ -1,49 +1,64 @@
 import pdfplumber
+import re
 
 
-def remove_label(text, label):
-    label_length = len(label)
-    removed_text = text[label_length:]
-    return removed_text.strip()
+def _split_camel_case(text):
+    return re.sub(r"(?<=\w)([A-Z])", r" \1", text)
 
 
 class Transcript:
     def __init__(self, path_to_pdf):
-        self.transcript = pdfplumber.open(path_to_pdf)
+        self.text = ""
 
+        with pdfplumber.open(path_to_pdf) as pdf:
+            for page in pdf.pages:
+                self.text += page.extract_text()
 
-    def get_student_name(self):
-        first_page_words = self._get_first_page_words()
+    def get_name(self):
+        match = re.search(r"^Name.*$", self.text, flags=re.MULTILINE)
+        name = match.group()
+        name = re.sub(r"Name: ", "", name)
+        return name
 
-        for word in first_page_words:
-            text = word['text']
-            if text.startswith("Name:"):
-                return remove_label(text, label="Name:")
+    def get_id(self):
+        match = re.search(r"^Student ID.*$", self.text, flags=re.MULTILINE)
+        stu_id = match.group()
+        stu_id = re.sub(r"Student ID: ", "", stu_id)
+        return stu_id
 
-        raise Exception("Student Name Not Found!")
+    def get_major(self):
+        section_title = r"^Program:.*Master"
+        major = r".*Major"
+        regex = section_title + r".*" + major
 
+        match = re.search(regex, self.text, flags=re.MULTILINE | re.DOTALL)
+        major = match.group()
+        major = re.search(r"^.*Major.*$", major, flags=re.MULTILINE).group()
+        major = re.sub(r".*: ", "", major).strip()
+        major = _split_camel_case(major)
 
-    def get_student_id(self):
-        first_page_words = self._get_first_page_words()
+        return major
 
-        for word in first_page_words:
-            text = word['text']
-            if text.startswith("Student ID:"):
-                return remove_label(text, label="Student ID:")
+    def get_beginning_of_graduate_record(self):
+        section_title_regex = r"^Beginning ?of ?Graduate ?Record"
+        semester_regex = r"\d{4} ?(Summer|Spring|Fall|Winter)"
+        regex = section_title_regex + r"\n" + semester_regex
+        match = re.search(regex, self.text, flags=re.MULTILINE)
+        semester = match.group()
+        semester = re.sub("^.*\n", "", semester)
+        semester = _split_camel_case(semester)
+        return semester
 
-        raise Exception("Student ID Not Found!")
+    def get_combined_cumulative_gpa(self):
+        section_title_regex = r"^Graduate ?Career ?Totals"
+        gpa_regex = r"Combined ?Cum ?GPA ?\d\.\d{3}"
+        regex = section_title_regex + r".*" + gpa_regex
+        match = re.search(regex, self.text, flags=re.MULTILINE | re.DOTALL)
+        match = match.group()
+        match = re.search(gpa_regex, match, flags=re.MULTILINE)
+        match = match.group()
+        gpa = re.sub(r"Combined ?Cum ?GPA ?", "", match)
+        return gpa
 
-
-    def close(self):
-        self.transcript.close()
-
-
-    def _get_first_page_words(self):
-        first_page = self._get_first_page()
-        first_page_words = first_page.extract_words(x_tolerance=30, keep_blank_chars=True)
-
-        return first_page_words
-
-
-    def _get_first_page(self):
-        return self.transcript.pages[0]
+    # https://stackoverflow.com/questions/199059/a-pythonic-way-to-insert-a-space-before-capital-letters
+    # Some pdfs don't split camel case. Mike Modano is an example
