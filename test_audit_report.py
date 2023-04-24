fe97ac47a075d63c3a1a4bbfacdf83b1c0fbe9ca
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-04-24 11:12:13 -0500

    test(audit_report): Add tests for IntelligentSystems and Systems

--- test_audit_report.py
+++ test_audit_report.py
@@ -1,5 +1,5 @@
 from audit_report import AuditReport
-from track import DataScience
+from track import DataScience, IntelligentSystems, Systems
 
 
 def assertEqual(a, b):
@@ -32,3 +32,15 @@ test_core(krusty_krab, krusty_krab_core)
 monica_geller = AuditReport("transcripts/monica-geller.pdf", DataScience())
 monica_geller_core = ["CS 6313", "CS 6350", "CS 6363", "CS 6375", "CS 6360"]
 test_core(monica_geller, monica_geller_core)
+
+mike_modano = AuditReport("transcripts/mike-modano.pdf", IntelligentSystems())
+mike_modano_core = ["CS 6320", "CS 6363", "CS 6364", "CS 6375", "CS 6360"]
+test_core(mike_modano, mike_modano_core)
+
+stevie_budd = AuditReport("transcripts/stevie-budd.pdf", IntelligentSystems())
+stevie_budd_core = ["CS 6320", "CS 6363", "CS 6364", "CS 6375", "CS 6360"]
+test_core(stevie_budd, stevie_budd_core)
+
+chandler_bing = AuditReport("transcripts/chandler-bing.pdf", Systems())
+chandler_bing_core = ["CS 6304", "CS 6363", "CS 6378", "CS 6396", "CS 6380"]
+test_core(chandler_bing, chandler_bing_core)
