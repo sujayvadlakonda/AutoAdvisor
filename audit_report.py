aa45d2aad96154a1140b85a1af1f75a5a885406a
Lisa Chen <bao.lisa.chen@gmail.com>
2023-05-07 07:38:57 -0500

    akelanda's copy of his outstanding req code

--- audit_report.py
+++ audit_report.py
@@ -90,6 +90,7 @@ class AuditReport:
         outstanding_requirements_section += str(self.get_outstanding_core_gpa()) + "\n"
         outstanding_requirements_section += str(self.get_outstanding_elec_gpa()) + "\n"
         outstanding_requirements_section += str(self.get_outstanding_overall_gpa()) + "\n"
+        print(outstanding_requirements_section)
         return outstanding_requirements_section
 
     # Returns core courses on transcript regardless of completion status
@@ -292,6 +293,7 @@ class AuditReport:
         num_of_remaining = 0
         total_gradepoints = 0
         gradepoints = 0
+        core_gpa_needed = 0.00
 
         for course in remaining_core_courses:
             num_of_remaining += 1
